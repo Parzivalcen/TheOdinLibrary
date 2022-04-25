90383c06e6ce8cb9e5061b758fbad3e3440bae1a
David Centurion <dencen9@gmail.com>
2022-04-25 05:10:20 -0400

    remove btn is working

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// get input values
+// list of books
 let books = [];
 function Book(title, author, pages, read) {
   this.title = title;
@@ -11,6 +11,8 @@ Book.prototype.info = function () {
   console.log(`${this.title}, by ${this.author}, ${this.pages}, ${this.read}`);
 };
 
+// cards container
+const cardsContainer = document.querySelector(".container--cards");
 // this will be on a button.
 function addBook() {
   // Takes user input
@@ -24,37 +26,54 @@ function addBook() {
     read = "Not read";
   }
   let book = new Book(title, author, pages, read);
-  books.push(book);
-  loop();
-}
-// Remove books
-function removeBook() {
-  const btnRemove = document.querySelector(".book-remove");
-  btnRemove.addEventListener("click", () => {});
+  // check if book exist on the list
+  if (!cardsContainer.innerHTML.includes(book.title)) {
+    books.push(book);
+    loop();
+  }
 }
 
 // CARDS
-// cards container
-const cardsContainer = document.querySelector(".container--cards");
-
+let count = 0;
 function loop() {
   books.forEach((book) => {
     const card = document.createElement("div");
+
     card.innerHTML = `
     <h2>${book.title}</h2>
     <h3>${book.author}</h3>
     <p>${book.pages}</p>
-    <p>${book.read}</p>
+    <button class="btn--read">${book.read}</button>
     <button class="book-remove">Remove</button>
     `;
+
     // IF the books is not already in the screen, add it.
     if (!cardsContainer.innerHTML.includes(book.title)) {
       cardsContainer.appendChild(card);
+      card.dataset.index = `${count}`;
+      count++;
     }
   });
 }
 // Envent listener
+// add books
 const sendBtn = document.querySelector("#addBook");
 sendBtn.addEventListener("click", () => {
   addBook();
+});
+
+// Remove books
+// task: get current array index
+
+/*
+- Selects the cards Container
+- listens for click on the document
+- if that click contains the book remvove class
+- the card is removed from the list and the DOM
+*/
+document.querySelector(".container--cards").addEventListener("click", (e) => {
+  if (e.target.classList.contains("book-remove")) {
+    books.splice(e.target.parentElement.dataset.index, 1);
+    e.target.parentElement.remove();
+  }
 });
