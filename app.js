9025bc17b7cad1b3cc14cc4283c0ec7656f72cc3
David Centurion <dencen9@gmail.com>
2022-04-26 05:10:25 -0400

    its looking good

--- app.js
+++ app.js
@@ -38,11 +38,11 @@ let count = 0;
 function loop() {
   books.forEach((book) => {
     const card = document.createElement("div");
-
+    card.classList.add("card");
     card.innerHTML = `
     <h2>${book.title}</h2>
     <h3>${book.author}</h3>
-    <p>${book.pages}</p>
+    <p>${book.pages} pages</p>
     <button class="btn--read">${book.read}</button>
     <button class="book-remove">Remove</button>
     `;
@@ -76,4 +76,43 @@ document.querySelector(".container--cards").addEventListener("click", (e) => {
     books.splice(e.target.parentElement.dataset.index, 1);
     e.target.parentElement.remove();
   }
+  // Change read status
+
+  if (e.target.classList.contains("btn--read")) {
+    if (e.target.innerText == "Book Read") {
+      e.target.innerText = "Not Read";
+    } else {
+      e.target.innerText = "Book Read";
+    }
+  }
 });
+
+// UI
+const addBookForm = document.querySelector(".new-book-btn");
+const booksAddContainer = document.querySelector(".add-Container");
+
+// SHOW form to add books when add new book is clicked
+addBookForm.addEventListener("click", () => {
+  const visibilily = booksAddContainer.getAttribute("data-visible");
+  if (visibilily == "false") {
+    booksAddContainer.setAttribute("data-visible", true);
+  }
+});
+
+// Make the form to add new book desappear when a new book is added.
+sendBtn.addEventListener("click", () => {
+  const visibilily = booksAddContainer.getAttribute("data-visible");
+  if (visibilily == "true") {
+    booksAddContainer.setAttribute("data-visible", false);
+  }
+});
+
+book1 = new Book("The scientist", "Coldplay", 21, "Book Read");
+books.push(book1);
+book2 = new Book("7 Habits", "IDK", 212, "Not Read");
+books.push(book2);
+book3 = new Book("12 rules", "JBP", 500, "Not Read");
+books.push(book3);
+book4 = new Book("DUNE", "Frank Herbert", 1200, "Read");
+books.push(book4);
+loop();
